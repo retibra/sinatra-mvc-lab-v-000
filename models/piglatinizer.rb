8c3017f71558f982cf08476c3491835c8c8b90ec
Timothy Ege <retibra@gmail.com>
2018-08-24 13:54:41 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,8 +16,8 @@ private
 
     if !consonant?(word[0])
       word = word + "w"
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
+  #  elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+  #    word = word.slice(3..-1) + word.slice(0,3)
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
     else
